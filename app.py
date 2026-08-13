5c45814f55eca9be380a91d342e59e78abf5de8c
fatihakin-ntbk <fatih.akin@sinav.k12.tr>
2026-08-13 10:55:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import bcrypt
 
-password = "AkademIQ2026!"
-
-hashed = bcrypt.hashpw(
-    password.encode(),
-    bcrypt.gensalt()
-).decode()
-
-st.code(hashed)
+st.code(
+    bcrypt.hashpw(
+        "AkademIQ2026!".encode(),
+        bcrypt.gensalt()
+    ).decode()
+)
